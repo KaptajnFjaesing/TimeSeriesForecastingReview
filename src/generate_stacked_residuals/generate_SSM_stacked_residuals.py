2c42e1f68fce2704dfb26830ec3858eac5d33924
ssholm <56428011+ssholm@users.noreply.github.com>
2024-10-01 12:50:20 +0200

    Added parallelisation to SSM and lgbm. Adjusted model parameters for TiDe.

--- src/generate_stacked_residuals/generate_SSM_stacked_residuals.py
+++ src/generate_stacked_residuals/generate_SSM_stacked_residuals.py
@@ -7,6 +7,7 @@ Created on Wed Sep 25 10:22:56 2024
 from tqdm import tqdm
 import pandas as pd
 import statsmodels.api as sm
+from joblib import Parallel, delayed
 
 from src.utils import suppress_output
 import src.generate_stacked_residuals.global_model_parameters as gmp
@@ -15,7 +16,7 @@ import src.generate_stacked_residuals.global_model_parameters as gmp
 model_config_default = {
     'freq_seasonal': [{
         'period': int(gmp.number_of_weeks_in_a_year),
-        'harmonics':15
+        'harmonics':10
         }],
     'autoregressive': 5,
     'level': True,
@@ -25,6 +26,20 @@ model_config_default = {
     'irregular': True
     }
 
+def generate_forecast(fh, df, forecast_horizon, model_config, time_series_column_group):
+    training_data = df.iloc[:-fh]
+    y_train_min = training_data[time_series_column_group].min()
+    y_train_max = training_data[time_series_column_group].max()
+    y_train = (training_data[time_series_column_group]-y_train_min)/(y_train_max-y_train_min)
+    model_forecasts = pd.DataFrame(columns = time_series_column_group)
+    for time_series_column in time_series_column_group:
+        struobs = sm.tsa.UnobservedComponents(y_train[time_series_column],**model_config)
+        mlefit = suppress_output(struobs.fit, disp = 0)
+        model_results = mlefit.get_forecast(forecast_horizon).summary_frame(alpha=0.05)
+        model_forecasts[time_series_column] = model_results['mean']*(y_train_max[time_series_column]-y_train_min[time_series_column])+y_train_min[time_series_column]
+    test_data = df[time_series_column_group].iloc[-fh:].head(forecast_horizon)
+    return (test_data-model_forecasts.set_index(df.iloc[-fh:].head(forecast_horizon).index)).reset_index(drop = True)
+
 def generate_SSM_stacked_residuals(
         df: pd.DataFrame = gmp.df,
         forecast_horizon: int = gmp.forecast_horizon,
@@ -32,20 +47,10 @@ def generate_SSM_stacked_residuals(
         model_config: dict = model_config_default
         ):
     time_series_column_group = [x for x in df.columns if 'HOUSEHOLD' in x]
-    residuals = []
-    for fh in tqdm(range(forecast_horizon,forecast_horizon+simulated_number_of_forecasts), desc = 'generate_SSM_stacked_residuals'):
-        training_data = df.iloc[:-fh]
-        y_train_min = training_data[time_series_column_group].min()
-        y_train_max = training_data[time_series_column_group].max()
-        y_train = (training_data[time_series_column_group]-y_train_min)/(y_train_max-y_train_min)
-        model_forecasts = pd.DataFrame(columns = time_series_column_group)
-        for time_series_column in time_series_column_group:
-            struobs = sm.tsa.UnobservedComponents(y_train[time_series_column],**model_config)
-            mlefit = suppress_output(struobs.fit, disp = 0)
-            model_results = mlefit.get_forecast(forecast_horizon).summary_frame(alpha=0.05)
-            model_forecasts[time_series_column] = model_results['mean']*(y_train_max[time_series_column]-y_train_min[time_series_column])+y_train_min[time_series_column]
-        test_data = df[time_series_column_group].iloc[-fh:].head(forecast_horizon)
-        residuals.append((test_data-model_forecasts.set_index(df.iloc[-fh:].head(forecast_horizon).index)).reset_index(drop = True))
+    residuals = Parallel(n_jobs=10)(delayed(generate_forecast)(
+        fh, df, forecast_horizon, model_config, time_series_column_group
+    ) for fh in tqdm(range(forecast_horizon, forecast_horizon + simulated_number_of_forecasts), desc='generate_SSM_stacked_residuals'))
+
     pd.concat(residuals, axis=0).to_pickle('./data/results/stacked_residuals_statespace.pkl')
 
 generate_SSM_stacked_residuals()
--- src/generate_stacked_residuals/generate_tide_darts_stacked_residuals.py
+++ src/generate_stacked_residuals/generate_tide_darts_stacked_residuals.py
@@ -2,13 +2,16 @@
 Created on Wed Sep 25 12:32:06 2024
 
 @author: Jonas Petersen
-"""
 
+TiDEModel paper:
+https://arxiv.org/pdf/2304.08424
+"""
 from tqdm import tqdm
 import pandas as pd
 import darts.models as dm
 from darts import TimeSeries
 import logging
+import torch
 
 logging.getLogger("pytorch_lightning").setLevel(logging.ERROR)
 
@@ -18,16 +21,25 @@ import src.generate_stacked_residuals.global_model_parameters as gmp
 
 model_config_default = {
     'input_chunk_length': 64,
-    'hidden_size': 64,
-    'n_epochs': 60,
-    'dropout': 0.4,
-    'optimizer_kwargs': {'lr': 0.0001},
+    'hidden_size': 256,
+    'n_epochs': 20,
+    'dropout': 0.3,
+    'optimizer_cls': torch.optim.AdamW,
+    'optimizer_kwargs': {'lr': 0.001, 'weight_decay': 1e-12, 'amsgrad': True},
     'random_state': 42,
     'num_encoder_layers': 2,
     'num_decoder_layers': 2,
-    'decoder_output_dim': 4
+    'decoder_output_dim': 8,
+    'temporal_decoder_hidden': 32,
+    'use_layer_norm': True,
+    'batch_size': 5,
     }
 
+"""
+NOTE:
+According to the Darts documentation, their deep learning models should have a parameter, where the number of CPU workers can be set.
+However, this does not seem to work. You should also be able to pass a parameter to the underlying PyTorch dataloader, but this also does not seem to work.
+"""
 def generate_tide_darts_stacked_residuals(
         df: pd.DataFrame = gmp.df,
         forecast_horizon: int = gmp.forecast_horizon,
@@ -39,7 +51,7 @@ def generate_tide_darts_stacked_residuals(
     for fh in tqdm(range(forecast_horizon,forecast_horizon+simulated_number_of_forecasts), desc = 'generate_tide_darts_stacked_residuals'):
         tide_model = dm.TiDEModel(**model_config, output_chunk_length = forecast_horizon)
         suppress_output(tide_model.fit, TimeSeries.from_dataframe(df.iloc[:-fh], 'date'))
-        predictions = suppress_output(tide_model.predict,forecast_horizon)
+        predictions = suppress_output(tide_model.predict, forecast_horizon)
         model_forecasts = pd.DataFrame(data = predictions.values(), columns = time_series_column_group)
         residuals.append((df.iloc[-fh:].head(forecast_horizon)[time_series_column_group]-model_forecasts.set_index(df.iloc[-fh:].head(forecast_horizon).index)).reset_index(drop = True))
     pd.concat(residuals, axis=0).to_pickle("./data/results/stacked_residuals_tide_darts.pkl")
